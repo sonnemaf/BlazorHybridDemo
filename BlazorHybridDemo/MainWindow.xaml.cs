8f52a6fd82b0ed0075368abb9ee471391106f386
sonnemaf <fons.sonnemans@reflectionit.nl>
2022-04-29 12:00:16 +0200

    Add project files.

---
+++ BlazorHybridDemo/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazorHybridDemo {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+            InitializeComponent();
+        }
+    }
+}
